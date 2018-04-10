ca6a526cbf90348b37e1a6ce4adc5f0e56a3fc86
eduardogm81qg <QuarkGluon123>
2018-04-10 12:53:57 +0200

    Uso de partials on HBS

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
 // Definimos el contenido público
 app.use(express.static( __dirname + '/public' ));
 
 // Express HBS Engine
+hbs.registerPartials( __dirname + '/views/partials' );
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
 
     res.render('home', { nombre: 'Fernando', anio: new Date().getFullYear()});
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {anio: new Date().getFullYear()});
 
 });
 
